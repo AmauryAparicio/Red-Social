8195f51ae27ed941c64a2f9f4111bd0f0d518414
Amaury <amaurysunstar@gmail.com>
2020-01-08 23:33:24 -0600

    Dia 1, Se crearon y adaptaron los modelos a usar.

---
+++ app/Like.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Like extends Model
+{
+    protected $table = 'likes';
+
+    //Relación de Muchos a Uno
+	public function user()
+	{
+			return $this->belongsTo('App\User','user_id');
+    }
+    
+    //Relación One To Many
+    public function image()
+    {
+        return $this->hasMany('App/Image', 'image_id');
+    }
+}
