2b20433e0db4125bf3b843be5d528954abd0dd21
Amaury <amaurysunstar@gmail.com>
2020-01-09 00:30:32 -0600

    Dia 1, se agrego el volcado de datos de prueba para la base de datos.

--- database/database.sql
+++ database/database.sql
@@ -22,6 +22,11 @@ CREATE TABLE IF NOT EXISTS users(
 
 ) CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE=InnoDb;
 
+/* Volcado de usuarios */
+INSERT INTO users VALUES(NULL, 'user', 'Amaury', 'Aparicio', 'Panfilo', 'amaury@krakenwd.com', 'pass', NULL, CURTIME(), CURTIME(), NULL);
+INSERT INTO users VALUES(NULL, 'user', 'Juan', 'Lopez', 'Juanin', 'juanin@krakenwd.com', 'pass', NULL, CURTIME(), CURTIME(), NULL);
+INSERT INTO users VALUES(NULL, 'user', 'Jaime', 'Duende', 'Jaimico', 'jaimin@krakenwd.com', 'pass', NULL, CURTIME(), CURTIME(), NULL);
+
 /* Creamos la tabla de imagenes */
 CREATE TABLE IF NOT EXISTS images(
 
@@ -35,6 +40,12 @@ CREATE TABLE IF NOT EXISTS images(
 	CONSTRAINT fk_images_user FOREIGN KEY(user_id) REFERENCES users(id)
 
 ) CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE=InnoDb;
+
+/*Volcado de imagenes*/
+INSERT INTO images VALUES(NULL, 1, 'test1.jpg', 'descripcion de pueba 1', CURTIME(), CURTIME());
+INSERT INTO images VALUES(NULL, 2, 'test2.jpg', 'descripcion de pueba 2', CURTIME(), CURTIME());
+INSERT INTO images VALUES(NULL, 3, 'test3.jpg', 'descripcion de pueba 3', CURTIME(), CURTIME());
+INSERT INTO images VALUES(NULL, 1, 'test4.jpg', 'descripcion de pueba 4', CURTIME(), CURTIME());
 
 /* Creamos la tabla de comentarios */
 CREATE TABLE IF NOT EXISTS comments(
@@ -51,6 +62,17 @@ CREATE TABLE IF NOT EXISTS comments(
 
 ) CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE=InnoDb;
 
+/* Volcado de comentarios */
+INSERT INTO comments VALUES(NULL, 1, 1, 'comentario de prueba 1', CURTIME(), CURTIME());
+INSERT INTO comments VALUES(NULL, 2, 1, 'comentario de prueba 2', CURTIME(), CURTIME());
+INSERT INTO comments VALUES(NULL, 3, 1, 'comentario de prueba 3', CURTIME(), CURTIME());
+INSERT INTO comments VALUES(NULL, 1, 2, 'comentario de prueba 4', CURTIME(), CURTIME());
+INSERT INTO comments VALUES(NULL, 2, 2, 'comentario de prueba 5', CURTIME(), CURTIME());
+INSERT INTO comments VALUES(NULL, 3, 2, 'comentario de prueba 6', CURTIME(), CURTIME());
+INSERT INTO comments VALUES(NULL, 1, 3, 'comentario de prueba 7', CURTIME(), CURTIME());
+INSERT INTO comments VALUES(NULL, 2, 3, 'comentario de prueba 8', CURTIME(), CURTIME());
+INSERT INTO comments VALUES(NULL, 3, 3, 'comentario de prueba 9', CURTIME(), CURTIME());
+
 /* Creamos la tabla de likes */
 CREATE TABLE IF NOT EXISTS likes(
 
@@ -64,3 +86,14 @@ CREATE TABLE IF NOT EXISTS likes(
 	CONSTRAINT fk_likes_image FOREIGN KEY(image_id) REFERENCES images(id)
 
 ) CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE=InnoDb;
+
+/* Volcado de likes */
+INSERT INTO likes VALUES(NULL, 1, 1, CURTIME(), CURTIME());
+INSERT INTO likes VALUES(NULL, 2, 1, CURTIME(), CURTIME());
+INSERT INTO likes VALUES(NULL, 3, 1, CURTIME(), CURTIME());
+INSERT INTO likes VALUES(NULL, 1, 2, CURTIME(), CURTIME());
+INSERT INTO likes VALUES(NULL, 2, 2, CURTIME(), CURTIME());
+INSERT INTO likes VALUES(NULL, 3, 2, CURTIME(), CURTIME());
+INSERT INTO likes VALUES(NULL, 1, 3, CURTIME(), CURTIME());
+INSERT INTO likes VALUES(NULL, 2, 3, CURTIME(), CURTIME());
+INSERT INTO likes VALUES(NULL, 3, 3, CURTIME(), CURTIME());
