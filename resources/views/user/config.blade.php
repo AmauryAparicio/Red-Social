c2465ca3d1fd0161408198685c4845efd23a27d8
AmauryAparicio <amaurysunstar@gmail.com>
2020-01-11 22:53:37 -0600

    Dia 3, se agrego el input para subir el avatar del usuario

--- resources/views/user/config.blade.php
+++ resources/views/user/config.blade.php
@@ -13,71 +13,92 @@
 					Configuracion de mi cuenta
 				</div>
 				<div class="card-body">
-					<form action=" {{route('user.update')}} " method="post" aria-label="Configuracion de mi cuenta">
+					<form action=" {{route('user.update')}} " method="post" enctype="multipart/form-data" aria-label="Configuracion de mi cuenta">
 						@csrf
 	
-							<div class="form-group row">
-								<label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
-	
-								<div class="col-md-6">
-									<input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ Auth::user()->name }}" required autocomplete="name" autofocus>
-	
-									@error('name')
-										<span class="invalid-feedback" role="alert">
-											<strong>{{ $message }}</strong>
-										</span>
-									@enderror
-								</div>
+						<div class="form-group row">
+							<label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+
+							<div class="col-md-6">
+								<input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ Auth::user()->name }}" required autocomplete="name" autofocus>
+
+								@error('name')
+									<span class="invalid-feedback" role="alert">
+										<strong>{{ $message }}</strong>
+									</span>
+								@enderror
 							</div>
-	
-							<div class="form-group row">
-								<label for="surname" class="col-md-4 col-form-label text-md-right">{{ __('Surname') }}</label>
-	
-								<div class="col-md-6">
-									<input id="Surname" type="text" class="form-control @error('Surname') is-invalid @enderror" name="surname" value="{{ Auth::user()->surname }}" required autocomplete="surname" autofocus>
-	
-									@error('surname')
-										<span class="invalid-feedback" role="alert">
-											<strong>{{ $message }}</strong>
-										</span>
-									@enderror
-								</div>
+						</div>
+
+						<div class="form-group row">
+							<label for="surname" class="col-md-4 col-form-label text-md-right">{{ __('Surname') }}</label>
+
+							<div class="col-md-6">
+								<input id="Surname" type="text" class="form-control @error('Surname') is-invalid @enderror" name="surname" value="{{ Auth::user()->surname }}" required autocomplete="surname" autofocus>
+
+								@error('surname')
+									<span class="invalid-feedback" role="alert">
+										<strong>{{ $message }}</strong>
+									</span>
+								@enderror
 							</div>
-	
-							<div class="form-group row">
-								<label for="nick" class="col-md-4 col-form-label text-md-right">{{ __('Nick') }}</label>
-	
-								<div class="col-md-6">
-									<input id="nick" type="text" class="form-control @error('nick') is-invalid @enderror" name="nick" value="{{ Auth::user()->nick }}" required autocomplete="nick" autofocus>
-	
-									@error('nick')
-										<span class="invalid-feedback" role="alert">
-											<strong>{{ $message }}</strong>
-										</span>
-									@enderror
+						</div>
+
+						<div class="form-group row">
+							<label for="nick" class="col-md-4 col-form-label text-md-right">{{ __('Nick') }}</label>
+
+							<div class="input-group col-md-6">
+								<div class="input-group-prepend">
+									<div class="input-group-text">@</div>
 								</div>
+								<input id="nick" type="text" class="form-control @error('nick') is-invalid @enderror" name="nick" value="{{ Auth::user()->nick }}" required autocomplete="nick" autofocus>
+
+								@error('nick')
+									<span class="invalid-feedback" role="alert">
+										<strong>{{ $message }}</strong>
+									</span>
+								@enderror
 							</div>
-	
-							<div class="form-group row">
-								<label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-	
-								<div class="col-md-6">
-									<input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ Auth::user()->email }}" required autocomplete="email">
-	
-									@error('email')
-										<span class="invalid-feedback" role="alert">
-											<strong>{{ $message }}</strong>
-										</span>
-									@enderror
-								</div>
+						</div>
+
+						<div class="form-group row">
+							<label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+
+							<div class="col-md-6">
+								<input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ Auth::user()->email }}" required autocomplete="email">
+
+								@error('email')
+									<span class="invalid-feedback" role="alert">
+										<strong>{{ $message }}</strong>
+									</span>
+								@enderror
 							</div>
-							<div class="form-group row mb-0">
-								<div class="col-md-6 offset-md-4">
-									<button type="submit" class="btn btn-primary">
-										Guardar cambios
-									</button>
-								</div>
-							</div>	
+						</div>
+
+						<div class="form-group row">
+							<label for="image_path" class="col-md-4 col-form-label text-md-right">Avatar</label>
+
+							<div class="col-md-6">
+								<input id="image_path" type="file" class="form-control-file @error('image_path') is-invalid @enderror" name="image_path" autocomplete="image_path" autofocus>
+								
+
+								@error('image_path')
+									<span class="invalid-feedback" role="alert">
+										<strong>{{ $message }}</strong>
+									</span>
+								@enderror
+							</div>
+						</div>
+
+
+						<div class="form-group row mb-0">
+							<div class="col-md-6 offset-md-4">
+								<button type="submit" class="btn btn-primary">
+									Guardar cambios
+								</button>
+							</div>
+						</div>
+							
 					</form>
 				</div>
 			</div>
