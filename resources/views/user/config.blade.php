da4fb3e5a99b0fcbfc0c40fc045378a53ca456a5
AmauryAparicio <amaurysunstar@gmail.com>
2020-01-13 00:16:07 -0600

    Dia 4, Se cambio la ubicacion del avatar en el formulario y se le aplicaron estilos, se mudo el codigo a un include

--- resources/views/user/config.blade.php
+++ resources/views/user/config.blade.php
@@ -76,15 +76,18 @@
 						</div>
 
 						<div class="form-group row">
-							@if (Auth::user()->image)
-									<img src="{{route('user.avatar', ['filename'=>Auth::user()->image])}}">
-							@endif
+							
 							<label for="image_path" class="col-md-4 col-form-label text-md-right">Avatar</label>
 
 							<div class="col-md-6">
-								<input id="image_path" type="file" class="form-control-file @error('image_path') is-invalid @enderror" name="image_path" autocomplete="image_path" autofocus>
+								<div class="w-75">
+									@include('includes.avatar')
+								</div>
 								
-
+								
+								<input id="image_path" type="file" class="form-control-file mt-2 @error('image_path') is-invalid @enderror" name="image_path" autocomplete="image_path" autofocus>
+								
+								
 								@error('image_path')
 									<span class="invalid-feedback" role="alert">
 										<strong>{{ $message }}</strong>
